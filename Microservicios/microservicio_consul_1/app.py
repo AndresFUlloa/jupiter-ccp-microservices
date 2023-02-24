a51428d03845214f21b9c2d3f54069d5717728bd
robertoamin <r.amin@uniandes.edu.co>
2023-02-23 21:49:22 -0500

    se reorganizan los modelos de los 3 microservicios de consulta

--- Microservicios/microservicio_consul_1/app.py
+++ Microservicios/microservicio_consul_1/app.py
@@ -1,6 +1,6 @@
 from microservicio_consul_1 import create_app
-from .modelos import db, Venta
-from .modelos import VentaSchema
+from .modelos import db, Venta, Vendedor, TipoDocumento
+from .modelos import VentaSchema, VendedorSchema
 from flask_restful import Api
 from .vistas import VistaVenta, VistaVentas
 
@@ -20,8 +20,13 @@ api.add_resource(VistaVenta, '/venta/<int:id_venta>')
 #prueba
 with app.app_context():
     venta_schema = VentaSchema()
-    v = Venta(nombre='Roberto Amin', direccion='la calleja', telefono='300200000', producto='bicicleta', pago='efectivo')
+    vendedor_schema = VendedorSchema()
+    r = Vendedor(nombre='Erick', apellido='Sanchez', tipo_documento=TipoDocumento.CEDULA, numero_documento='10040004', telefono='3004040400')
+    v = Venta(producto='Bicicleta', cantidad=5, cliente='Roberto Amin', vendedor=r.id)
+    r.ventas.append(v)
     db.session.add(v)
+    db.session.add(r)
     db.session.commit()
     print([venta_schema.dumps(venta) for venta in Venta.query.all()])
+    print([vendedor_schema.dumps(vendedor) for vendedor in Vendedor.query.all()])
 """
--- Microservicios/microservicio_consul_3/app.py
+++ Microservicios/microservicio_consul_3/app.py
@@ -1,6 +1,6 @@
 from microservicio_consul_3 import create_app
-from .modelos3 import db, Venta
-from .modelos3 import VentaSchema
+from .modelos3 import db, Venta, Vendedor, TipoDocumento
+from .modelos3 import VentaSchema, VendedorSchema
 from flask_restful import Api
 from .vistas3 import VistaVenta, VistaVentas
 
@@ -12,16 +12,22 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
+
 api3 = Api(app)
 api3.add_resource(VistaVentas, '/ventas')
 api3.add_resource(VistaVenta, '/venta/<int:id_venta>')
-"""
 
+"""
 #prueba
 with app.app_context():
     venta_schema = VentaSchema()
-    v = Venta(nombre='Roberto Amin', direccion='la calleja', telefono='300200000', producto='bicicleta', pago='efectivo')
+    vendedor_schema = VendedorSchema()
+    r = Vendedor(nombre='Erick', apellido='Sanchez', tipo_documento=TipoDocumento.CEDULA, numero_documento='10040004', telefono='3004040400')
+    v = Venta(producto='Bicicleta', cantidad=5, cliente='Roberto Amin', vendedor=r.id)
+    r.ventas.append(v)
     db.session.add(v)
+    db.session.add(r)
     db.session.commit()
     print([venta_schema.dumps(venta) for venta in Venta.query.all()])
+    print([vendedor_schema.dumps(vendedor) for vendedor in Vendedor.query.all()])
 """
