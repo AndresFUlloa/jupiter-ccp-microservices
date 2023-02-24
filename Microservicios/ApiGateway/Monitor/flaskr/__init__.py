d66c3fd259b8febb332297371f72ae812fda091e
robertoamin <r.amin@uniandes.edu.co>
2023-02-24 16:34:09 -0500

    se agrega un monitor para los microservicios de consulta

--- Microservicios/ApiGateway/Monitor/flaskr/__init__.py
+++ Microservicios/ApiGateway/Monitor/flaskr/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import os
+import platform
+import socket
+import struct
+import sys
+import time
+from typing import Tuple
+
 
 def create_app(config_name):
     app = Flask(__name__)
--- Microservicios/ApiGateway/Monitor/flaskr/monitor.py
+++ Microservicios/ApiGateway/Monitor/flaskr/monitor.py
@@ -1,3 +1,4 @@
+
 import os
 import platform
 import socket
@@ -5,28 +6,40 @@ import struct
 import sys
 import time
 from typing import Tuple
-from ping3 import ping
 
-# Set the target IP address or hostname of the microservice you want to monitor
-target = "example.com"
+from requests import get, exceptions
 
-# Set the interval in seconds between each ping request
-interval = 1
+# Monitor de microservicios a traves de request
+def monitoreo(target: str, tipo:str):
+    try:
+        respuesta=get(target, timeout=3)
+        print("El microservicio {} en linea...codigo:".format(tipo), respuesta.status_code)
+    except exceptions.ConnectionError:
+        print("El microservicio {} esta fuera de servicio, codigo: 400".format(tipo))
 
+
+
+"""
 def icmp_ping(target: str) -> bool:
-    """
-    Ping the target using ICMP and return True if successful, False otherwise.
-    """
+
+# Ping the target using ICMP and return True if successful, False otherwise.
+
     response_time = ping(target, timeout=2)
+    print("el tiempo de respuesta da....", response_time)
     return response_time is not None
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+
+def monitoreo(target: str):
     print(f"Pinging {target} every {interval} seconds...")
     while True:
         try:
             is_alive = icmp_ping(target)
+            print(is_alive)
             print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {target} is {'up' if is_alive else 'down'}")
             time.sleep(interval)
         except KeyboardInterrupt:
             print("Stopping ICMP monitoring...")
             break
+
+"""
