53cc8597c0809fe4cdaf7cfcebd82e21e6af2590
AndresFUlloa <a.ulloar@uniandes.edu.co>
2023-02-25 20:14:04 -0500

    Fix Monitor

--- Microservicios/ApiGateway/Monitor/flaskr/monitor.py
+++ Microservicios/ApiGateway/Monitor/flaskr/monitor.py
@@ -15,11 +15,28 @@ servidor3_down = False
 
 # Monitor de microservicios a traves de request
 def monitoreo(target: str, tipo:str):
+    global servidor1_down
+    global servidor2_down
+    global servidor3_down
+
     try:
         respuesta=get(target, timeout=3)
+        last_digit = 1
+        if servidor1_down and tipo == 'principal':
+            servidor1_down = False
+        elif servidor2_down and tipo == 'redundante 1':
+            servidor2_down = False
+            last_digit = 2
+        response = post('http://127.0.0.1:5006/api/new_url', data={'new_url': 'http://127.0.0.1:500' + last_digit})
         print("El microservicio {} en linea...codigo:".format(tipo), respuesta.status_code)
     except exceptions.ConnectionError:
         print("El microservicio {} esta fuera de servicio, codigo: 400".format(tipo))
         # le comunicamos al API Gateway que no use mas este microservicio porque esta abajo
-        response = post('http://127.0.0.1:5006/api/new_url', data = {'new_url':'http://127.0.0.1:5002'})
+        last_digit = 2
+        if not servidor1_down and tipo == 'principal':
+            servidor1_down = True
+        elif not servidor2_down and tipo == 'redundante 1':
+            servidor2_down = True
+            last_digit = 3
 
+        response = post('http://127.0.0.1:5006/api/new_url', data={'new_url': 'http://127.0.0.1:500' + last_digit})
