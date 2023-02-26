78f59ab9fc925137fcc156ec08005978d134c8ae
robertoamin <r.amin@uniandes.edu.co>
2023-02-26 16:17:19 -0500

    incluimos automatizacion de shutdown de microservicios

--- Microservicios/microservicio_consul_2/app.py
+++ Microservicios/microservicio_consul_2/app.py
@@ -3,7 +3,8 @@ from .modelos2 import db, Venta, Vendedor, TipoDocumento
 from .modelos2 import VentaSchema, VendedorSchema
 from flask_restful import Api
 from .vistas2 import VistaVenta, VistaVentas
-
+from flask import request
+from flask import Flask
 
 app = create_app('default')
 app_context = app.app_context()
@@ -12,23 +13,17 @@ app_context.push()
 db.init_app(app)
 db.create_all()
 
-
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug server')
+    func()
 
 api2 = Api(app)
 api2.add_resource(VistaVentas, '/ventas')
 api2.add_resource(VistaVenta, '/venta/<int:id_venta>')
 
-"""
-#prueba
-with app.app_context():
-    venta_schema = VentaSchema()
-    vendedor_schema = VendedorSchema()
-    r = Vendedor(nombre='Erick', apellido='Sanchez', tipo_documento=TipoDocumento.CEDULA, numero_documento='10040004', telefono='3004040400')
-    v = Venta(producto='Bicicleta', cantidad=5, cliente='Roberto Amin', vendedor=r.id)
-    r.ventas.append(v)
-    db.session.add(v)
-    db.session.add(r)
-    db.session.commit()
-    print([venta_schema.dumps(venta) for venta in Venta.query.all()])
-    print([vendedor_schema.dumps(vendedor) for vendedor in Vendedor.query.all()])
-"""
