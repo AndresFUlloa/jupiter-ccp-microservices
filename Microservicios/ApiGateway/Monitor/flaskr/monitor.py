99af4c0d4caf537e5f0a530d43588918696e9024
robertoamin <r.amin@uniandes.edu.co>
2023-02-26 21:28:13 -0500

    se agrega archivo log de pruebas

--- Microservicios/ApiGateway/Monitor/flaskr/monitor.py
+++ Microservicios/ApiGateway/Monitor/flaskr/monitor.py
@@ -27,6 +27,7 @@ def monitoreo(target: str, tipo:str):
         lista_url = None
         if servidor1_down and tipo == 'principal':
             servidor1_down = False
+            current_url = 'http://127.0.0.1:5001'
             lista_url = {'new_url': current_url}
             LogUtils.write_message('Server: {} is up'.format(target))
         elif servidor2_down and tipo == 'redundante 1':
@@ -44,8 +45,7 @@ def monitoreo(target: str, tipo:str):
             LogUtils.write_message('Se cambio el servidor: {}'. format(current_url))
         print("El microservicio {} en linea...codigo:".format(tipo), respuesta.status_code)
     except exceptions.ConnectionError:
-        if target == (current_url + "/ventas"):
-            LogUtils.write_message('Server: {} is down'.format(target))
+
         print("El microservicio {} esta fuera de servicio, codigo: 400".format(tipo))
        # le comunicamos al API Gateway que no use mas este microservicio porque esta abajo
         lista_url = None
