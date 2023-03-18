ba8c18c3abddfbef811ba535e072cbb1085fef1e
robertoamin <r.amin@uniandes.edu.co>
2023-03-18 18:24:28 -0500

    se arregla las conexiones a la cola

--- Microservicios/cola_mensajes/task.py
+++ Microservicios/cola_mensajes/task.py
@@ -1,7 +1,8 @@
 import pika
 import json
+import requests
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.URLParameters(('amqps://rxrfnebd:p2RNcqtzpqchcVfPZ2gKd-uyCX1Q3yM0@jackal.rmq.cloudamqp.com/rxrfnebd')))
 channel = connection.channel()
 
 queue = channel.queue_declare('inventario_notify')
@@ -19,7 +20,10 @@ channel.queue_bind(
     )
 def callback(ch, method, properties, body):
     payload=json.loads(body)
-    print(' [X] Notificando al Inventario ', payload)
+    print(payload)
+    encabezados = {'Content-Type':'application/json'}
+    response = requests.post('http://localhost:5008/actualizar_venta/' + str(payload['venta_id']), data=json.dumps(payload),headers=encabezados)
+    print(' [X] Notificando al Inventario ', payload, response)
     print(' [X] Done')
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
--- Microservicios/cola_mensajes/utiles/exchange_util.py
+++ Microservicios/cola_mensajes/utiles/exchange_util.py
@@ -7,17 +7,24 @@ class ExchangeUtil:
 
     @staticmethod
     def start_connection():
-        ExchangeUtil.connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        ExchangeUtil.connection = pika.BlockingConnection(pika.URLParameters(('amqps://rxrfnebd:p2RNcqtzpqchcVfPZ2gKd-uyCX1Q3yM0@jackal.rmq.cloudamqp.com/rxrfnebd')))
         ExchangeUtil.channel = ExchangeUtil.connection.channel()
+        queue = ExchangeUtil.channel.queue_declare('inventario_notify')
+        queue_name = queue.method.queue
+        ExchangeUtil.channel.queue_bind(
+            exchange='orden',
+            queue=queue_name,
+            routing_key='inventario.notify'
+        )
 
     @staticmethod
     def send_message(json_request):
-        ExchangeUtil.channel.basic_publish(
+        ver_publicacion=ExchangeUtil.channel.basic_publish(
             exchange='orden',
             routing_key='inventario.notify',
             body = json.dumps(json_request)
         )
-        print(' [X] Notificacion enviada a la bodega')
+        print(' [X] Notificacion enviada a la bodega de Jesus', ver_publicacion)
 
     @staticmethod
     def close_connection():
